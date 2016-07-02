8d14a993abe1aadd0e37cd92e497fe35ebc3ca0f
jytang <jytang@users.noreply.github.com>
2016-07-02 04:33:11 -0700

    Let's do this again.. (#3)
    Basic class skeletons

---
+++ lib/reachy.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'json'
+require 'pp'
+
+require_relative 'reachy/game'
+require_relative 'reachy/round'
+
+##############################################
+# TODO: Main functionality
+##############################################
+module Reachy
+  puts "No problem!";               # tmp debug...
+
+end
