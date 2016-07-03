2e769c6658895723cab1448d71d558ef9e283269
Kainu <kainu.ht@gmail.com>
2016-07-03 03:49:40 -0700

    Filled out Game class

--- lib/reachy/game.rb
+++ lib/reachy/game.rb
@@ -1,5 +1,11 @@
+require 'json'
+require 'date'
+require 'pp'
+
+require_relative 'round'
+
 ##############################################
-# TODO: Scoreboard record class
+# Scoreboard record class
 ##############################################
 class Game
 
@@ -11,42 +17,88 @@ class Game
   attr_accessor :scoreboard       # List of round records
 
   # Initialize game
+  # Param: db - hash of game data
+  # Populate Game object with info from hash
   def initialize(db)
+    @filename = db["filename"]
+    @created_at = DateTime.parse(db["created_at"])
+    @last_updated = DateTime.parse(db["last_updated"])
+    @mode = db["mode"]
+    @players = db["players"]
+    @scoreboard = []
+    db["scoreboard"].each do |round|
+      @scoreboard << Round.new(round)
+    end
   end
 
-  # Read from file
+  # Read JSON database file and delegate to initialize() to repopulate object
   def read_data
+    file = File.read("../data/" + @filename + ".json")
+    db = JSON.parse(file)
+    self.initialize(db)
   end
 
-  # Write to file
+  # Write Game object to JSON database file
   def write_data
+    hash = self.instance_variables.each_with_object({}) \
+      { |var, h| h[var.to_s.delete("@")] = self.instance_variable_get(var) }
+    File.open("../data/" + @filename + ".json", "w") do |f|
+      f.write(JSON.pretty_generate(hash))
+    end
   end
 
   # Add new round result
+  # Param: round - hash of new round record
+  # Append given round to scoreboard
   def add_round(round)
+    @scoreboard << round
+    self.write_data
   end
 
-  # Remove latest round
+  # Remove latest round from scoreboard
   def remove_last_round
+    @scoreboard.pop
+    self.write_data
   end
 
   # Add riichi stick declared by player
+  # Param: player - string of player's name
   def add_riichi(player)
+    @scoreboard.last.add_riichi(player)
+  end
+
+  # Print scoreboard header
+  # Round       Joshua    Kenta     Thao
+  def print_header
+    #maxlen = [@players.max_by(&:length).length, 5].max
+    printf "%-#{COL_SPACING}s", "Round"
+    @players.each do |p|
+      printf "%-#{COL_SPACING}s", p
+    end
+    puts nil
   end
 
   # Print entire scoreboard
   def print_scoreboard
+    self.print_header
+    @scoreboard.each do |r|
+      r.print_scores
+    end
   end
 
   # Print last round scores
   # Round       Joshua    Kenta     Thao
   # E1B1R2      33400     39800     31800
   def print_last_round
+    self.print_header
+    @scoreboard.last.print_scores
   end
 
-  # Print 1-line summary of game
-  # game1: 3P (E1B1R1) ~ Joshua, Kenta, Thao ~ 2016-11-05 14:05:00
-  def to_string
+  # Print 1-line game title
+  # game1: 3P (E1B1R1) ~ Joshua, Kenta, Thao ~ 2016-11-05T14:05:00-07:00
+  def print_title
+    printf "%s: %dP (%s) ~ %s ~ %s", @filename, @mode, @scoreboard.last.name,
+      @players.join(", "), @last_updated
   end
 
 end
--- lib/reachy/round.rb
+++ lib/reachy/round.rb
@@ -1,16 +1,26 @@
+COL_SPACING = 15
+
 ##############################################
 # TODO: Round record class
 ##############################################
 class Round
 
-  attr_accessor :name           # Name of round (e.g. E1B2R1)
+  attr_reader   :name           # Name of round (e.g. E1B2R1)
   attr_reader   :wind           # Round wind
+  attr_reader   :number         # Round number
   attr_reader   :bonus          # Bonus sticks at beginning of round
   attr_accessor :riichi         # Riichi sticks available before round ends
   attr_accessor :scores         # Hash of <player's name> => <score>
 
   # Initialize round
+  # Param: round - hash of round data
+  # Populate Round object with info from hash
   def initialize(round)
+  end
+
+  # Create a deep copy
+  def clone
+    return Marshal.load(Marshal.dump(self))
   end
 
   # Add riichi stick declared by player
