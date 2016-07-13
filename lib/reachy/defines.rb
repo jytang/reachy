0737d52c059077ae327f8e47c19eb8b0ea51b3b1
Joshua Tang <joshua.y.tang@gmail.com>
2016-07-12 18:56:25 -0700

    Put everything in Reachy module

--- lib/reachy/defines.rb
+++ lib/reachy/defines.rb
@@ -1,12 +1,14 @@
-# Scoreboard formatting
-COL_SPACING = 15
+module Reachy
+  # Scoreboard formatting
+  COL_SPACING = 15
 
-# Round result types
-T_TSUMO   = 1
-T_RON     = 2
-T_TENPAI  = 3
-T_NOTEN   = 4
-T_CHOMBO  = 5
+  # Round result types
+  T_TSUMO   = 1
+  T_RON     = 2
+  T_TENPAI  = 3
+  T_NOTEN   = 4
+  T_CHOMBO  = 5
 
-# List of named hand values
-L_HANDS   = ["mangan","haneman","baiman","sanbaiman","yakuman"]
+  # List of named hand values
+  L_HANDS   = ["mangan","haneman","baiman","sanbaiman","yakuman"]
+end
